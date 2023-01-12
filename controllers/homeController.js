ac15a47b9fdfd486c3b3aed2b51e5fe0209ebb19
vabigor <vab.igor@mail.ru>
2023-01-12 15:20:02 +0300

    homeController

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,15 +1,9 @@
-// const commentService = require("../service/commentService");
+const commentService = require("../service/commentService");
 
 class HomeController {
 
     async get(req, res){
-        // await commentService.getAll(req, res, true);
-        // res.json({ASD:"123"})
-
-        res.render("object.hbs", {
-            title: "ВольтЭра",
-            head: "ВольтЭра"
-        });
+        await commentService.getAll(req, res, true);
     }
 }
 
