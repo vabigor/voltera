65f7d440049435d9c154e6d6e7530e1bceacc086
vabigor <vab.igor@mail.ru>
2023-01-12 12:07:53 +0300

    3

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,7 +4,12 @@ class HomeController {
 
     async get(req, res){
         // await commentService.getAll(req, res, true);
-        res.json({ASD:"123"})
+        // res.json({ASD:"123"})
+
+        res.render("object.hbs", {
+            title: "ВольтЭра",
+            head: "ВольтЭра"
+        });
     }
 }
 
--- index.js
+++ index.js
@@ -12,32 +12,35 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
-// hbs.registerPartials(__dirname + "/views/partials");
-// app.engine("hbs", expressHbs.engine(
-//     {
-//         layoutsDir: "views/layouts",
-//         defaultLayout: "layout",
-//         extname: "hbs"
-//     }
-// ))
-// app.set("view engine", "hbs");
-//
-// Handlebars.registerHelper('times', function(n, block) {
-//     let accum = '';
-//     for(let i = 0; i < n; i++)
-//         accum += block.fn(i);
-//     return accum;
-// });
-// Handlebars.registerHelper('ifCond', function(v1, v2, options) {
-//     if(v1 === v2) {
-//         return options.fn(this);
-//     }
-//     return options.inverse(this);
-// });
-// Handlebars.registerHelper("inc", function(value) {
-//     let x = value;
-//     return parseInt(x) + 1;
-// });
+hbs.registerPartials(__dirname + "/views/partials");
+app.engine("hbs", expressHbs.engine(
+    {
+        layoutsDir: "views/layouts",
+        defaultLayout: "layout",
+        extname: "hbs"
+    }
+))
+app.set("view engine", "hbs");
+
+Handlebars.registerHelper('times', function(n, block) {
+    let accum = '';
+    for(let i = 0; i < n; i++)
+        accum += block.fn(i);
+    return accum;
+});
+Handlebars.registerHelper('ifCond', function(v1, v2, options) {
+    if(v1 === v2) {
+        return options.fn(this);
+    }
+    return options.inverse(this);
+});
+Handlebars.registerHelper("inc", function(value) {
+    let x = value;
+    return parseInt(x) + 1;
+});
+Handlebars.registerHelper("url", function(value) {
+    return process.env.URL+"/"+value
+});
 // Handlebars.registerHelper("url", function(value) {
 //     if (value[0]==="/"){
 //         value.slice(1,1)
@@ -49,6 +52,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/', router)
 
+// app.use(express.static('static'))
 app.use(express.static(path.join(__dirname, 'static')))
 // app.use(express.json()) *
 // app.use(express.static(path.resolve(__dirname, 'static'))) *
