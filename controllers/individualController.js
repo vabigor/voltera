fcdf4f255d81fe8473cd9f12029d1b3c33fde07c
vabigor <vab.igor@mail.ru>
2023-01-23 16:18:47 +0300

    refactoring

--- controllers/individualController.js
+++ controllers/individualController.js
@@ -12,12 +12,6 @@ class IndividualController {
         });
     }
 
-    async emergencyTechnicalWork(req, res){
-        res.render("individual/emergency_technical_work.hbs", {
-            title: "Аварийно-технические работы"
-        });
-    }
-
     async wiring(req, res){
         res.render("individual/wiring.hbs", {
             title: "Электромонтаж (квартиры, коттеджи)"
--- routes/individualRouter.js
+++ routes/individualRouter.js
@@ -4,7 +4,6 @@ const individualController = require('../controllers/individualController')
 
 router.get('/project', individualController.project)
 router.get('/electrophysical-measurements', individualController.electrophysicalMeasurements)
-router.get('/emergency-technical-work', individualController.emergencyTechnicalWork)
 router.get('/wiring', individualController.wiring)
 router.get('/network-connection', individualController.networkConnection)
 router.get('/accompaniment', individualController.accompaniment)
